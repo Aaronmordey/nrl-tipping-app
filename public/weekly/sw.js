bfa34619a0b986ac7c9c08a7e983acd7eaad302c
Aaronmordey <aaronmordey1@gmail.com>
2026-06-10 11:45:19 +1000

    Add files via upload

--- public/weekly/sw.js
+++ public/weekly/sw.js
@@ -1,7 +1,5 @@
 self.addEventListener("install", () => self.skipWaiting());
 self.addEventListener("activate", (event) => {
-  event.waitUntil(
-    caches.keys().then(keys => Promise.all(keys.map(key => caches.delete(key)))).then(() => self.clients.claim())
-  );
+  event.waitUntil(self.clients.claim());
 });
 self.addEventListener("fetch", () => {});
